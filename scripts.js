256d2a870f899e7e87fc9ff15541781bd982031f
mcardacci <marc.cardacci@gmail.com>
2015-08-25 16:47:49 -0400

    creates view for single blog

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 //first backbone model!!
 var Blog = Backbone.Model.extend({
-	defaults: {
-		author: '', 
-		title: '',
-		url: ''
-	}
+  defaults: {
+    author: '', 
+    title: '',
+    url: ''
+  }
 });
 
 //first collection!! (array of models)
@@ -12,16 +12,60 @@ var Blogs = Backbone.Collection.extend({});
 
 //instantiate two Blogs
 var blog1 = new Blog({
-	author: "marco",
-	title: "marco's blog",
-	url: "http://mcardacci.github.io"
+  author: "marco",
+  title: "marco's blog",
+  url: "http://mcardacci.github.io"
 })
 
 var blog2 = new Blog({
-	author: "John",
-	title: "John's blog",
-	url: "http://www.johnsblog.com"
+  author: "John",
+  title: "John's blog",
+  url: "http://www.johnsblog.com"
 });
 
 //instantiate a collection
 var blogs = new Blogs([blog1, blog2]);
+
+//backbone view for one blog
+var BlogView = Backbone.View.extend({
+  model: new Blog(),
+  tagName: 'tr',
+  initialize: function() {
+    this.template = _.template($('blogs-list-template').html());
+  },
+  render: function() {
+    this.$el.html(this.template( this.model.toJSON() ))
+  }
+});
+
+//backbone view for all blogs
+var BlogsView = Backbone.View.extend({
+  model: blogs,
+  el: $('.blogs-list'),
+  initialize: function() {
+    this.model.on('add', this.render(), this);
+  },
+  render: function() {
+    var self = this;
+    this.$el.html('');
+    _.each(this.model.toArray, function(blog) {
+      self.$el
+    })
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
